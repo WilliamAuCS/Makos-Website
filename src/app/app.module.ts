0b469544b456f400891fd60535de8b87703364b0
William Au <WilliamAuCS@Gmail.com>
2020-06-01 19:30:41 -0700

    Created toolbar and toolbar hyperlinks

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterModule } from '@angular/router';
+import { ToolbarComponent } from './toolbar/toolbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ToolbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule, 
+    MatToolbarModule, 
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent }, 
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
