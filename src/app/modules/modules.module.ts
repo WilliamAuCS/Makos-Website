966cfd00f39c737858748f1638688f41bb591cb4
William Au <WilliamAuCS@Gmail.com>
2020-07-15 17:25:42 -0700

    Reformatted Homepage mobile view to use [ngClass] for lower DOM load

--- src/app/modules/modules.module.ts
+++ src/app/modules/modules.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthenticationModule } from './authentication/authentication.module';
-
+import { HomeModule } from './home/home.module';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule, 
     AuthenticationModule, 
+    HomeModule,
   ]
 })
 export class ModulesModule { }
