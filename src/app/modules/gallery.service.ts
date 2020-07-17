ff75f22b83b8a9779778f9e93ae559cdace80716
William Au <WilliamAuCS@Gmail.com>
2020-07-16 17:12:45 -0700

    Updated routing link

--- src/app/modules/gallery.service.ts
+++ src/app/modules/gallery.service.ts
@@ -6,7 +6,7 @@ import { HttpClient } from '@angular/common/http';
 })
 export class GalleryService {
 
-  private _galleryURL = "http://localhost:3000/api/gallery/category1";
+  private _galleryURL = "http://localhost:3000/api/gallery";
 
   constructor(private http: HttpClient) { }
 
