1d18bd98efe305a4a7ecde1ef539e2f8ee43314f
William Au <WilliamAuCS@Gmail.com>
2021-07-18 21:40:09 -0700

    Added env paths

--- src/app/modules/authentication/auth.service.ts
+++ src/app/modules/authentication/auth.service.ts
@@ -1,23 +1,12 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
-
-  private _registerURL = "https://server.makosusa.com/api/register";
-  private _loginUrl = "https://server.makosusa.com/api/login";
-  private _deleteAccountUrl = "https://server.makosusa.com/api/user/";
-  private _verification = "https://server.makosusa.com/api/verification/";
-
-  // FOR TESTING PURPOSES ONLY
-  // private _registerURL = "http://localhost:8080/api/register";
-  // private _loginUrl = "http://localhost:8080/api/login";
-  // private _deleteAccountUrl = "http://localhost:8080/api/user/";
-  // private _verification = "http://localhost:8080/api/verification/";
-
   public isLoggedIn: boolean;
 
   // Used to handle register errors
@@ -28,58 +17,61 @@ export class AuthService {
   public login_error: boolean = false;
   public login_error_response: String;
 
-  constructor(private http: HttpClient,
-    private _router: Router) {
+  constructor(private http: HttpClient, private _router: Router) {
     this.isLoggedIn = this.loggedIn();
   }
 
-
   // Sends http POST request to backend api url containing user credentials
-  registerUser(user: { email: string, password: string }) {
+  registerUser(user: { email: string; password: string }) {
     // Register user to db and return response details as observable
-    this.http.post<{ token: string, payloadEmail: string }>(this._registerURL, user)
+    this.http
+      .post<{ token: string; payloadEmail: string }>(
+        environment._registerURL,
+        user
+      )
       // Subscribing to observable
       .subscribe(
         // Log either response or error
-        res => {
+        (res) => {
           this.register_error = false;
           this.logginProcess(res);
         },
-        err => {
-          if (err.error == "Invalid Email Format") {
+        (err) => {
+          if (err.error == 'Invalid Email Format') {
             this.register_error = true;
-            this.register_error_response = "Invalid Email Format";
-          }
-          else if (err.error == "Email in use") {
+            this.register_error_response = 'Invalid Email Format';
+          } else if (err.error == 'Email in use') {
             this.register_error = true;
-            this.register_error_response = "Email already in use";
+            this.register_error_response = 'Email already in use';
           }
           console.log(err);
-        },
-
-      )
+        }
+      );
   }
 
   // Sends http POST request to backend api url containing user credentials
   loginUser(user) {
-    this.http.post<{ token: String, payloadEmail: String}>(this._loginUrl, user)
+    this.http
+      .post<{ token: String; payloadEmail: String }>(
+        environment._loginUrl,
+        user
+      )
       // Subscribing to observable
       .subscribe(
         // Log either response or error
-        res => {
+        (res) => {
           this.login_error = false;
           this.logginProcess(res);
         },
-        err => {
-          if (err.error == "Invalid Email Format") {
+        (err) => {
+          if (err.error == 'Invalid Email Format') {
             this.login_error = true;
-            this.login_error_response = "Invalid email format";
-          }
-          else if(err.error == "Invalid credentials") {
+            this.login_error_response = 'Invalid email format';
+          } else if (err.error == 'Invalid credentials') {
             this.login_error = true;
-            this.login_error_response = "Invalid credentials";
+            this.login_error_response = 'Invalid credentials';
           }
-          console.log(err)
+          console.log(err);
         }
       );
   }
@@ -87,9 +79,9 @@ export class AuthService {
   // Standard user loggin process
   logginProcess(res) {
     // Setting JWT token in cookie
-    this.setCookie("token", res.token, 365);
+    this.setCookie('token', res.token, 365);
     // Encoding user email with base64 and setting in cookie
-    this.setCookie("usem", btoa(res.payloadEmail), 365);
+    this.setCookie('usem', btoa(res.payloadEmail), 365);
     this.isLoggedIn = true;
     // Navigation to homepage
     this._router.navigate(['/']);
@@ -98,46 +90,47 @@ export class AuthService {
   // Logging out the user
   logoutUser() {
     // Removing cookies (2)
-    this.setCookie("token", "", 0);
-    this.setCookie("usem", "", 0);
+    this.setCookie('token', '', 0);
+    this.setCookie('usem', '', 0);
     this.isLoggedIn = false;
     // Navigation to homepage
-    this._router.navigate(["/"]);
+    this._router.navigate(['/']);
   }
 
   loggedIn() {
-    return !!this.getCookie("token");
+    return !!this.getCookie('token');
   }
 
   getToken() {
-    return this.getCookie("token");
+    return this.getCookie('token');
   }
 
   deleteAccount() {
-    this.http.delete<{ statusCode: String }>(this._deleteAccountUrl + this.getEmail())
+    this.http
+      .delete<{ statusCode: String }>(
+        environment._deleteAccountUrl + this.getEmail()
+      )
       .subscribe(
-        res => {
-          if (res.statusCode == "200") {
-            console.log("Acount deleted");
+        (res) => {
+          if (res.statusCode == '200') {
+            console.log('Acount deleted');
             this.logoutUser();
           }
         },
-        err => {
+        (err) => {
           console.error(err);
         }
       );
   }
 
   verifyToken() {
-    return this.http.get<any>(this._verification)
-    
+    return this.http.get<any>(environment._verification);
   }
 
   // Retrieves email from cookie and decodes using base64
   getEmail() {
-    return atob(this.getCookie("usem"));
+    return atob(this.getCookie('usem'));
   }
-
 
   /* 
   Cookie API found at https://www.w3schools.com/js/js_cookies.asp
@@ -147,16 +140,16 @@ export class AuthService {
   setCookie(cname, cvalue, exdays) {
     // Creating date object
     var d = new Date();
-    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-    var expires = "expires=" + d.toUTCString();
+    d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
+    var expires = 'expires=' + d.toUTCString();
     // Creating cookie in browser
     // ** ADD ;secure;httpOnly when testing is complete **
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";secure;path=/";
+    document.cookie = cname + '=' + cvalue + ';' + expires + ';secure;path=/';
   }
 
   // Cookie retreval
   getCookie(cname) {
-    var name = cname + "=";
+    var name = cname + '=';
     // Creating array to hold cookie parts split off of ';'
     var ca = document.cookie.split(';');
     // Looping through array
@@ -169,7 +162,6 @@ export class AuthService {
         return c.substring(name.length, c.length);
       }
     }
-    return "";
+    return '';
   }
-
 }
